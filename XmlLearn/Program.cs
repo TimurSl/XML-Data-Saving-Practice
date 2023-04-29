c963b288acbe24882f282388d2e3e6f8ec2c89ac
Timur Slinko <timurslinko1@gmail.com>
2023-04-29 11:42:28 +0300

    Added list type to data

--- XmlLearn/Program.cs
+++ XmlLearn/Program.cs
@@ -5,6 +5,8 @@ public class DataToSave
 	public string Name { get; set; }
 	public int Age { get; set; }
 	public string Address { get; set; }
+	
+	public List<string> Friends { get; set; } = new List<string>();
 }
 
 class Program
@@ -41,6 +43,9 @@ class Program
 		data.Name = "John";
 		data.Age = 25;
 		data.Address = "Some address";
+		data.Friends.Add("Friend 1");
+		data.Friends.Add("Friend 2");
+		data.Friends.Add("Friend 3");
 		
 		XmlSerializer serializer = new XmlSerializer(typeof(DataToSave));
 		using (FileStream stream = new FileStream("data.xml", FileMode.Create))
@@ -66,6 +71,7 @@ class Program
 			Console.WriteLine("Name: {0}", data.Name);
 			Console.WriteLine("Age: {0}", data.Age);
 			Console.WriteLine("Address: {0}", data.Address);
+			Console.WriteLine("Friends: {0}", string.Join(", ", data.Friends));
 		}	
 	}
 }
